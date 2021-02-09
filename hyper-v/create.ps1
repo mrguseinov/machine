97353ca56c1da88b469e6a8e1a9058ad9ac30908
Farid Guseinov <22242004+mrguseinov@users.noreply.github.com>
2021-02-09 18:53:10 +0300

    Download Ubuntu Server 20.04.2

--- hyper-v/create.ps1
+++ hyper-v/create.ps1
@@ -124,7 +124,7 @@ Set-Location $VmPath
 Write-Host " Done." @Green
 
 Write-Host "Downloading installation media (iso file)..." -NoNewline
-$IsoUrl = "https://releases.ubuntu.com/20.04.1/ubuntu-20.04.1-live-server-amd64.iso"
+$IsoUrl = "https://releases.ubuntu.com/20.04.2/ubuntu-20.04.2-live-server-amd64.iso"
 $IsoFileName = $IsoUrl.Split("/")[-1]
 $BitsTransferParams = @{
     Source      = $IsoUrl
@@ -137,7 +137,7 @@ Write-Host " Done." @Green
 
 Write-Host "Checking '$IsoFileName' hash..." -NoNewline
 $ActualHash = (Get-FileHash $IsoFileName -Algorithm "SHA256").Hash
-$ExpectedHash = "443511f6bf12402c12503733059269a2e10dec602916c0a75263e5d990f6bb93"
+$ExpectedHash = "D1F2BF834BBE9BB43FAF16F9BE992A6F3935E65BE0EDECE1DEE2AA6EB1767423"
 If ($ActualHash -Ne $ExpectedHash) {
     Write-Host
     Write-Host "Something went wrong. Hashes do not match!" @Warning
