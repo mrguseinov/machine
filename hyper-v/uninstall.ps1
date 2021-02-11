e1ed3ab26e8eef4caf7bf7bf2849eefade650e91
Farid Guseinov <22242004+mrguseinov@users.noreply.github.com>
2021-02-11 16:25:45 +0300

    Improve uninstall user experience

--- hyper-v/uninstall.ps1
+++ hyper-v/uninstall.ps1
@@ -24,16 +24,10 @@ If (Test-DecisionNotPositive $Decision) {
 Write-Host
 
 Write-Host "Finding the virtual machine '$VmName'..." -NoNewline
-Try {
-    $Machine = Get-VM $VmName
-    Write-Host " Done." @Green
-}
-Catch {
-    Write-Host " Error." @Red
-    Write-Host $_.Exception.Message @Warning
-}
-
+$Machine = Get-VM $VmName -ErrorAction "SilentlyContinue"
 If ($Null -Ne $Machine) {
+    Write-Host " Done." @Green
+
     Write-Host "Is the machine '$VmName' turned off?" -NoNewline
     If ($Machine.State -Ne "Off") {
         Write-Host " No." @Red
@@ -65,69 +59,163 @@ If ($Null -Ne $Machine) {
     }
 }
 Else {
+    Write-Host " Skip." @Green
+    Write-Host "The machine was not found." @Warning
     Write-Host
 }
 
 Write-Host "Deleting the '$SwitchName' vSwitch..." -NoNewline
-Try {
-    Remove-VMSwitch -Name $SwitchName -Force
-    Write-Host " Done." @Green
+$Switch = Get-VMSwitch $SwitchName -ErrorAction "SilentlyContinue"
+If ($Null -Ne $Switch) {
+    Try {
+        Remove-VMSwitch -Name $SwitchName -Force
+        Write-Host " Done." @Green
+    }
+    Catch {
+        Write-Host " Error." @Red
+        Write-Host $_.Exception.Message @Warning
+    }
 }
-Catch {
-    Write-Host " Error." @Red
-    Write-Host $_.Exception.Message @Warning
+Else {
+    Write-Host " Skip." @Green
+    Write-Host "The switch was not found." @Warning
 }
 
 Write-Host "Deleting the '$NatName' NAT..." -NoNewline
-Try {
-    Remove-NetNat -Name $NatName -Confirm:$False -ErrorAction "Stop"
-    Write-Host " Done." @Green
+$Nat = Get-NetNat $NatName -ErrorAction "SilentlyContinue"
+If ($Null -Ne $Nat) {
+    Try {
+        Remove-NetNat -Name $NatName -Confirm:$False -ErrorAction "Stop"
+        Write-Host " Done." @Green
+    }
+    Catch {
+        Write-Host " Error." @Red
+        Write-Host $_.Exception.Message @Warning
+    }
 }
-Catch {
-    Write-Host " Error." @Red
-    Write-Host $_.Exception.Message @Warning
+Else {
+    Write-Host " Skip." @Green
+    Write-Host "The NAT was not found." @Warning
 }
 
 Write-Host "Deleting the '$VmPath' folder..." -NoNewline
-Set-ParentLocationOrHome $VmPath
-Try {
-    Remove-Item -Path $VmPath -Recurse
-    Write-Host " Done." @Green
+If (Test-Path $VmPath -PathType "Container") {
+    If (Test-PathsEqual $VmPath (Get-Location).Path) {
+        Set-ParentLocationOrHome $VmPath
+    }
+
+    Try {
+        Remove-Item -Path $VmPath -Recurse
+        Write-Host " Done." @Green
+    }
+    Catch {
+        Write-Host " Error." @Red
+        Write-Host $_.Exception.Message @Warning
+    }
 }
-Catch {
-    Write-Host " Error." @Red
-    Write-Host $_.Exception.Message @Warning
+Else {
+    Write-Host " Skip." @Green
+    Write-Host "The folder was not found." @Warning
 }
 
-$ConfigFolderPath = "$HOME\.ssh"
-Write-Host "Deleting the key(s) from '$ConfigFolderPath'..." -NoNewline
-$KeyFilesPath = "$ConfigFolderPath\$VmName"
-Try {
-    $ItemsCountBefore = Get-NumberOfItemsInFolder $ConfigFolderPath
-    Remove-Item -Path "$KeyFilesPath*"
-    $ItemsCountAfter = Get-NumberOfItemsInFolder $ConfigFolderPath
-    If ($ItemsCountBefore -Eq $ItemsCountAfter) {
-        Throw "No files (keys) have been deleted."
+$SSHFolderPath = "$HOME\.ssh"
+Write-Host "Deleting the keys from '$SSHFolderPath'..." -NoNewline
+$SSHKeysPath = "$SSHFolderPath\$VmName"
+If (Test-Path "$SSHKeysPath*" -PathType "Leaf") {
+    $ItemsCountBefore = Get-NumberOfItemsInFolder $SSHFolderPath
+    Remove-Item -Path "$SSHKeysPath*"
+    $ItemsCountAfter = Get-NumberOfItemsInFolder $SSHFolderPath
+
+    If ($ItemsCountBefore -Gt $ItemsCountAfter) {
+        Write-Host " Done." @Green
     }
-    Write-Host " Done." @Green
+    Else {
+        Write-Host " Error." @Red
+        Write-Host "The keys have not been deleted." @Warning
+    }
 }
-Catch {
-    Write-Host " Error." @Red
-    Write-Host $_.Exception.Message @Warning
+Else {
+    Write-Host " Skip." @Green
+    Write-Host "The keys were not found." @Warning
 }
 
-$ConfigFilePath = "$ConfigFolderPath\config"
-Write-Host "Deleting the config from '$ConfigFilePath'..." -NoNewline
-Try {
-    $LinesCountBefore = (Get-Content $ConfigFilePath).Length
-    Remove-HostFromSHHConfig $ConfigFilePath $VMAddress
-    $LinesCountAfter = (Get-Content $ConfigFilePath).Length
-    If ($LinesCountBefore -Eq $LinesCountAfter) {
-        Throw "No configuration has been deleted."
+$SSHConfigFilePath = "$SSHFolderPath\config"
+Write-Host "Deleting the config from '$SSHConfigFilePath'..." -NoNewline
+If (Test-Path $SSHConfigFilePath -PathType "Leaf") {
+    If (Test-StringInFile $VMAddress $SSHConfigFilePath) {
+        $LinesCountBefore = (Get-Content $SSHConfigFilePath).Length
+        Remove-HostFromSHHConfig $VMAddress $SSHConfigFilePath
+        $LinesCountAfter = (Get-Content $SSHConfigFilePath).Length
+
+        If ($LinesCountBefore -Gt $LinesCountAfter) {
+            Write-Host " Done." @Green
+        }
+        Else {
+            Write-Host " Error." @Red
+            Write-Host "The configuration has not been deleted." @Warning
+        }
     }
-    Write-Host " Done." @Green
+    Else {
+        Write-Host " Skip." @Green
+        Write-Host "The '$VMAddress' hostname was not found." @Warning
+    }
 }
-Catch {
-    Write-Host " Error." @Red
-    Write-Host $_.Exception.Message @Warning
+Else {
+    Write-Host " Skip." @Green
+    Write-Host "The file was not found." @Warning
+}
+
+$KnownHostsFilePath = "$SSHFolderPath\known_hosts"
+Write-Host "Deleting the host from '$KnownHostsFilePath'..." -NoNewline
+If (Test-Path $KnownHostsFilePath -PathType "Leaf") {
+    If (Test-StringInFile $VMAddress $KnownHostsFilePath) {
+        $LinesCountBefore = (Get-Content $KnownHostsFilePath).Length
+        Remove-HostFromKnownHosts $VMAddress $KnownHostsFilePath
+        $LinesCountAfter = (Get-Content $KnownHostsFilePath).Length
+
+        If ($LinesCountBefore -Gt $LinesCountAfter) {
+            Write-Host " Done." @Green
+        }
+        Else {
+            Write-Host " Error." @Red
+            Write-Host "The host has not been deleted." @Warning
+        }
+    }
+    Else {
+        Write-Host " Skip." @Green
+        Write-Host "The '$VMAddress' host was not found." @Warning
+    }
+}
+Else {
+    Write-Host " Skip." @Green
+    Write-Host "The file was not found." @Warning
+}
+
+Write-Host "Deleting the '$SSHFolderPath' folder (if empty)..." -NoNewline
+If (Test-Path $SSHFolderPath -PathType "Container") {
+    If (Test-PathsEqual $SSHFolderPath (Get-Location).Path) {
+        Set-ParentLocationOrHome $SSHFolderPath
+    }
+
+    $FolderIsEmpty = (Get-NumberOfItemsInFolder $SSHFolderPath) -Eq 0
+    $FolderHasOnlyEmptyFiles = Test-FolderHasOnlyEmptyFiles $SSHFolderPath
+    If ($FolderIsEmpty -Or $FolderHasOnlyEmptyFiles) {
+        Remove-Item -Path $SSHFolderPath -Recurse
+
+        If (Test-Path $SSHFolderPath -PathType "Container") {
+            Write-Host " Error." @Red
+            Write-Host "The folder has not been deleted." @Warning
+        }
+        Else {
+            Write-Host " Done." @Green
+        }
+    }
+    Else {
+        Write-Host " Skip." @Green
+        Write-Host "The folder is not empty." @Warning
+    }
+}
+Else {
+    Write-Host " Skip." @Green
+    Write-Host "The folder was not found." @Warning
 }
